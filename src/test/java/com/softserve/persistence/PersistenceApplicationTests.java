d403791f515283865475b1dacc7f5f673b8cf119
Mykola Klushyn <mklushyn@atlassian.com>
2023-04-28 21:33:40 +0200

    Init repo and add first task

---
+++ src/test/java/com/softserve/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
